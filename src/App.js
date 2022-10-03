b989510765bb463ef1ee7a392a88c2e9b9100473
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2022-10-03 17:56:18 -0400

    Connected to spotify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import logo from './logo.svg';
+import * as dotenv from 'dotenv'
+import {useEffect, useState} from "react";
 import './App.css';
+import axios from 'axios';
+
+/*
+http://localhost:3000/#access_token=BQD0LuS8PrElDkHlCWoH7dLRqzuaEMCOHWbjch4UtyF8_1APdIOMCib4tfkbDlBaszIMSn7ggDtbQzg-Ddy7wSx33aV2qkp-dAKCFtQA5lgVmF_d_ufThJ0IC7HMG9lfzhxoB8g3VPEGpQ9s49F-0m_w3pNt0McokCipWJCQkHhqnYM&token_type=Bearer&expires_in=3600
+*/
+
+dotenv.config()
 
 function App() {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault(); 
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    console.log(data);
+    setArtists(data.artists.items);
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => {
+      return <div key={artist.id}>
+        {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No image</div>}
+        {artist.name}
+      </div>
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spotify React</h1>
+        {!token ? 
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a> 
+          : <button onClick={logout}>Logout</button>
+        }
+
+        {token ?
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+          </form>
+          : <h2>Please login</h2>
+        }
+
+        {renderArtists()}
+        
       </header>
     </div>
   );
