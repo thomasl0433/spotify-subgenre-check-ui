b1b5e3762f3b85aa4ab30cc3da11a81064fa0c8f
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2022-10-04 23:36:01 -0400

    moe

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ HC_more(Highcharts) //init module
 
 function App() {
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const REDIRECT_URI = "http://localhost:3000";
+  const REDIRECT_URI = "https://spotify-subgenre.vercel.app"
+  // const REDIRECT_URI = "http://localhost:3000";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
