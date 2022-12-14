49eefb956753a0b2c11b92048d4f762d618ae367
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2022-12-13 19:43:05 -0500

    emojis

--- src/components/headerInfo.js
+++ src/components/headerInfo.js
@@ -6,8 +6,8 @@ export default function HeaderInfo() {
 
     return (
         <div className="bg-gray-200 rounded-xl p-8 m-10 w-1/2">
-            <h3 className="font-bold text-gray-700 mb-2">Note:</h3>
-            <p>While in development, you may need to be added to a list to be able to view your
+            <h3 className="font-bold text-gray-700 mb-2">🚨 Note:</h3>
+            <p className="text-gray-500">While in development 🚧, you may need to be added to a list to be able to view your
                 data. Please reach out to Thomas to be added if you are experiencing errors
             </p>
         </div>
